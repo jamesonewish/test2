eec330dd556eb02e40f75b3cbdd71f412ade93de
jamesonewish <32664535+jamesonewish@users.noreply.github.com>
2018-01-16 20:20:11 +0700

    t
    t

--- RTCMultiConnection.js
+++ RTCMultiConnection.js
@@ -3889,13 +3889,26 @@
 
     // IceServersHandler.js
 
-    var IceServersHandler = (function() {
-        function getIceServers(connection) {
-            var iceServers = [];
+    //var IceServersHandler = (function() {
+       // function getIceServers(connection) {
+            //var iceServers = [];
 
-           
+            //iceServers.push(getSTUNObj('stun:stun.l.google.com:19302'));
 
-            return iceServers;
+            //iceServers.push(getTURNObj('stun:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
+            //iceServers.push(getTURNObj('turn:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
+            //iceServers.push(getTURNObj('turn:webrtcweb.com:8877', 'muazkh', 'muazkh')); // coTURN
+
+            //iceServers.push(getTURNObj('turns:webrtcweb.com:7788', 'muazkh', 'muazkh')); // coTURN
+            iceServers.push(getTURNObj('turns:webrtcweb.com:8877', 'muazkh', 'muazkh')); // coTURN
+
+
+            // check if restund is still active: http://webrtcweb.com:4050/
+            //iceServers.push(getTURNObj('stun:webrtcweb.com:4455', 'muazkh', 'muazkh')); // restund
+            //iceServers.push(getTURNObj('turn:webrtcweb.com:4455', 'muazkh', 'muazkh')); // restund
+            //iceServers.push(getTURNObj('turn:webrtcweb.com:5544?transport=tcp', 'muazkh', 'muazkh')); // restund
+
+            //return iceServers;
         }
 
         function getSTUNObj(stunStr) {
@@ -4912,7 +4925,7 @@
             if (useWebKit) {
                 servers = {
                     iceServers: [{
-                        urls: ''
+                        urls: 'stun:stun.services.mozilla.com'
                     }]
                 };
 
